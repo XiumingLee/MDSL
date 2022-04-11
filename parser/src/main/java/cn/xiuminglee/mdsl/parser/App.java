cfd7ddba36b8cf709116013dc0d912e626ededc9
Xiuming Lee <lxmcoder@163.com>
2022-04-11 19:22:32 +0800

    add LICENSE

--- parser/src/main/java/cn/xiuminglee/mdsl/parser/App.java
+++ parser/src/main/java/cn/xiuminglee/mdsl/parser/App.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2022, Xiuming Lee.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * Please contact me via email lxmcoder@163.com if you need additional information
+ * or have any questions.
+ */
+
 package cn.xiuminglee.mdsl.parser;
 
 import org.springframework.boot.SpringApplication;
